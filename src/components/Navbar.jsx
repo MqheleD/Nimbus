816bd0f942137cee0a53558ced244d40e798a268
MqheleD <ndabedd@gmail.com>
2026-01-10 17:44:26 +0200

    Powered by

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,15 @@ export default function Navbar() {
     return (
         /* 'fixed bottom-0' pins it to the bottom. 
            'w-full' ensures it spans the screen width. */
-        <section className="fixed bottom-0 left-0 w-full bg-none z-50">
-            <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+        <section className="fixed bottom-0 left-0 right-0 w-full bg-none z-50">
+            <div className="container w-full mx-auto px-6 py-4 flex justify-between items-center">
                 {/* Wrapping the brand name in a Link is best practice */}
                 <Link to="/" className="font-bold text-l">
                     Nimbus
                 </Link>
-                
+                <div>
+                    Powered by <Link to="https://openai.com" className="font-semibold text-gray-700 hover:text-blue-500 transition duration-300">QORSYN</Link>
+                </div>
                 <nav>
                     <ul className="flex space-x-6"> 
                         <li> 
