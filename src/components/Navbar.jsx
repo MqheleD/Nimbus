55c0b6f18ef318176cc25c05ae222cc979ecc735
MqheleD <ndabedd@gmail.com>
2026-01-12 12:16:44 +0200

    IDK

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <section className="fixed bottom-0 left-0 right-0 w-full bg-none z-50">
+    <section className="fixed bottom-0 left-0 right-0 w-full bg-black z-50">
       <div className="relative container w-full mx-auto px-6 py-4 flex justify-between items-center">
         {/* Brand */}
-        <Link to="/" className="font-bold text-lg">
+        <Link to="/" className="font-bold text-lg text-white hover:text-blue-500 transition duration-300">
           Nimbus
         </Link>
 
         {/* Centered "Powered by" */}
         <div className="absolute left-1/2 transform -translate-x-1/2">
-          <span>
+          <span className="text-gray-300 hover:text-blue-500 transition duration-300">
             Powered by{" "}
             <Link
               to=""
