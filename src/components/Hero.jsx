23f8b437d418be6439cda95b8df9cd8823bb82f7
MqheleD <ndabedd@gmail.com>
2026-01-13 16:10:11 +0200

    gradient shift

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <section className="bg-gradient-to-r from-red-400 to-blue-500 text-white py-20">
+        <section className="bg-gradient-to-r from-green-400 to-blue-500 text-white py-20">
             <div className="container mx-auto px-6 text-center">
                 <h1 className="text-4xl md:text-6xl font-bold mb-10">Nimbus</h1>
                 {/* <p className="text-lg md:text-2xl mb-8">Your gateway to seamless cloud solutions</p> */}
